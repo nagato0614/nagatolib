2a58d686832e68e07358c0c58c227c130dedef7f
nagato0614 <nagato0614@gmail.com>
2019-06-24 02:31:08 +0900

    [update]ヘッダーの拡張子を.hppに変更namespaceの衝突を避けるため

--- src/nagato.cpp
+++ src/nagato.cpp
@@ -2,10 +2,8 @@
 // Created by nagato0614 on 2019-06-22.
 //
 
-#include "nagato.h"
+#include "nagato.hpp"
 
 namespace nagato {
-	using Vector4f = Vector<float, 4>;
-	using Vector3f = Vector<float, 3>;
-	using Vector2f = Vector<float, 2>;
+
 }
