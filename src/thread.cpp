3eff544756d0ec928edb5bb5d89a2d9e5b1c92cb
nagato0614 <nagato0614@gmail.com>
2019-07-19 01:20:41 +0900

    [update]threadの動作を変更
    condition_variable_をwhileで囲んでいたが削除し,condition一つで動作できるようにした

--- src/thread.cpp
+++ src/thread.cpp
@@ -6,66 +6,63 @@
 
 namespace nagato {
 ThreadPool::ThreadPool(_size nthreads) noexcept
-	: nthreads(nthreads) {
-  assert(nthreads > 0);
+		: nthreads(nthreads) {
+	assert(nthreads > 0);
 
-  for (int i = 0; i < nthreads; i++) {
-	threads_.emplace_back(std::thread([this]() {
-	  while (true) {
+	for (int i = 0; i < nthreads; i++) {
+		threads_.emplace_back(std::thread([this]() {
+			while (true) {
+				_unique_lock lock(mutex_);
+				condition_variable_.wait(lock, [this]() {
+					return (is_work_ && !queue_.empty()) || !is_work_;
+				});
 
-		_unique_lock lock(mutex_);
-		while (is_work_ && queue_.empty()) {
-		  condition_variable_.wait(lock);
-		}
+				if (!is_work_ && queue_.empty())
+					return;
 
-		if (!is_work_ && queue_.empty())
-		  return;
+				_process work = std::move(queue_.front());
+				queue_.pop();
+				lock.unlock();
 
-		_process work = std::move(queue_.front());
-		queue_.pop();
-		lock.unlock();
-
-		work();
-	  }
-	}));
-  }
+				work();
+			}
+		}));
+	}
 }
 
 ThreadPool::~ThreadPool() noexcept {
-  Finish();
+	Finish();
 }
 
 void ThreadPool::Loop(std::function<void(_size)> &&f,
-					  ThreadPool::_size size) noexcept {
-  std::vector<std::future<void>> vector;
+											ThreadPool::_size size) noexcept {
+	std::vector<std::future<void>> vector;
 
-  for (int i = 0; i < size; i++) {
-	vector.push_back(
-		EnqueuTask(f, i)
-	);
-  }
+	for (int i = 0; i < size; i++) {
+		vector.push_back(EnqueuTask(f, i));
+	}
 
-  condition_variable_.notify_all();
+	condition_variable_.notify_all();
 
-  for (auto &i : vector) {
-	if (i.valid())
-	  i.get();
-  }
+	for (auto &i : vector) {
+		if (i.valid())
+			i.get();
+	}
 }
 
 void ThreadPool::Finish() noexcept {
-  is_work_ = false;
-  int joinable = 0;
-  condition_variable_.notify_all();
+	is_work_ = false;
+	int joinable = 0;
+	condition_variable_.notify_all();
 
-  // すべてのスレッドが終了するまでjoinを行う
-  do {
-	joinable = 0;
-	for (auto &thread : threads_)
-	  if (thread.joinable())
-		thread.join();
-	  else
-		joinable++;
-  } while (joinable != nthreads);
+	// すべてのスレッドが終了するまでjoinを行う
+	do {
+		joinable = 0;
+		for (auto &thread : threads_)
+			if (thread.joinable())
+				thread.join();
+			else
+				joinable++;
+	} while (joinable != nthreads);
 }
 }
--- src/thread.hpp
+++ src/thread.hpp
@@ -20,80 +20,80 @@ namespace nagato {
  * タスクを受け取って実行結果を受け取るfutureを返す
  */
 class ThreadPool {
-  using _thread_pool = std::vector<std::thread>;
-  using _size = std::size_t;
-  using _process = std::function<void(void)>;
-  using _queue = std::queue<_process>;
-  using _unique_lock = std::unique_lock<std::mutex>;
+	using _thread_pool = std::vector<std::thread>;
+	using _size = std::size_t;
+	using _process = std::function<void(void)>;
+	using _queue = std::queue<_process>;
+	using _unique_lock = std::unique_lock<std::mutex>;
 
  public:
 
-  explicit
-  ThreadPool(
-	  _size nthreads = std::thread::hardware_concurrency()
-  ) noexcept;
+	explicit
+	ThreadPool(
+			_size nthreads = std::thread::hardware_concurrency()
+	) noexcept;
 
-  ~ThreadPool() noexcept;
+	~ThreadPool() noexcept;
 
-  /**
-   * 受け取ったworkをqueueに追加する
-   * @tparam F
-   * @tparam Args
-   * @param f
-   * @param args
-   */
-  template<typename F, typename... Args>
-  void EnqueuWork(F &&f, Args &&... args) noexcept {
-    _unique_lock lock(mutex_);
-	queue_.push([=]() { f(args...); });
-	condition_variable_.notify_all();
-  }
-
-  /**
-   * 受け取ったタスクをqueueに追加する
-   * futureを返す
-   * @tparam F
-   * @tparam Args
-   * @param f
-   * @param args
-   * @return
-   */
-  template<typename F, typename... Args>
-  auto EnqueuTask(F &&f, Args &&...args)
-  -> std::future<std::invoke_result_t<F, Args...>> {
-	using return_type = std::invoke_result_t<F, Args...>;
-
-	auto task
-		= std::make_shared<std::packaged_task<return_type()>>(
-			std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-		);
-
-	std::future<return_type> res
-		= task->get_future();
-	{
-	  _unique_lock lock(mutex_);
-	  queue_.push([task]() { (*task)(); });
+	/**
+	 * 受け取ったworkをqueueに追加する
+	 * @tparam F
+	 * @tparam Args
+	 * @param f
+	 * @param args
+	 */
+	template<typename F, typename... Args>
+	void EnqueuWork(F &&f, Args &&... args) noexcept {
+		_unique_lock lock(mutex_);
+		queue_.push([=]() { f(args...); });
+		condition_variable_.notify_all();
 	}
-	condition_variable_.notify_all();
-	return res;
-  }
 
-  /**
-   * 指定した回数fを実行する
-   * @param f
-   * @param size
-   */
-  void Loop(std::function<void(_size)> &&f, _size size) noexcept;
+	/**
+	 * 受け取ったタスクをqueueに追加する
+	 * futureを返す
+	 * @tparam F
+	 * @tparam Args
+	 * @param f
+	 * @param args
+	 * @return
+	 */
+	template<typename F, typename... Args>
+	auto EnqueuTask(F &&f, Args &&...args)
+	-> std::future<std::invoke_result_t<F, Args...>> {
+		using return_type = std::invoke_result_t<F, Args...>;
 
-  void Finish() noexcept;
+		auto task
+				= std::make_shared<std::packaged_task<return_type()>>(
+						std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+				);
+
+		std::future<return_type> res
+				= task->get_future();
+		{
+			_unique_lock lock(mutex_);
+			queue_.push([task]() { (*task)(); });
+		}
+		condition_variable_.notify_all();
+		return res;
+	}
+
+	/**
+	 * 指定した回数fを実行する
+	 * @param f
+	 * @param size
+	 */
+	void Loop(std::function<void(_size)> &&f, _size size) noexcept;
+
+	void Finish() noexcept;
 
  private:
-  _thread_pool threads_;
-  _queue queue_;
-  bool is_work_ = true;
-  _size nthreads;
-  std::mutex mutex_;
-  std::condition_variable condition_variable_;
+	_thread_pool threads_;
+	_queue queue_;
+	bool is_work_ = true;
+	_size nthreads;
+	std::mutex mutex_;
+	std::condition_variable condition_variable_;
 };
 }
 #endif //NAGATOLIB_SRC_THREAD_HPP_
