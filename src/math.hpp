13582c27c0562399250307b39caf86dbba3873be
nagato0614 <nagato0614@gmail.com>
2019-08-15 01:43:56 +0900

    [clear]math.hppの関数をすべて定義と宣言を同時に同時に記述

--- src/math.hpp
+++ src/math.hpp
@@ -14,119 +14,95 @@ namespace nagato {
 
 // -----------------------------------------------------------------------------
 // function definition
-template<typename Type>
-constexpr Type sqrt(Type s) noexcept;
-
-template<typename Type>
-constexpr Type abs(Type s) noexcept;
-
-template<typename Return, typename L, typename R>
-constexpr Return max(L l, R r) noexcept;
-
-template<typename Return, typename L, typename R>
-constexpr Return min(L l, R r) noexcept;
-
-template<typename Type>
-constexpr bool is_nan(Type t) noexcept;
-
-template<typename Return, typename L, typename R>
-constexpr Return clamp(Return val, L low, R high) noexcept;
-
-template<typename Type>
-constexpr Type calc_Pi(int iteration) noexcept;
-
-// -----------------------------------------------------------------------------
-// Math Constant
-
-template<typename Type = float>
-constexpr Type Pi = calc_Pi<Type>(10);
 
 // -----------------------------------------------------------------------------
 // reference : https://cpplover.blogspot.com/2010/11/blog-post_20.html
-
 template<typename Type>
 constexpr Type sqrt(Type s) noexcept {
-  STATIC_ASSERT_IS_ARITHMETRIC(Type);
-  Type x = s / 2.0; // Is there any better way to determine initial value?
-  Type last_x = 0.0; // the value one before the last step
-  while (x != last_x) // until the difference is not significant
-  { // apply Babylonian method step
-	last_x = x;
-	x = (x + s / x) / 2.0;
-  }
-  return x;
+	STATIC_ASSERT_IS_ARITHMETRIC(Type);
+	Type x = s / 2.0; // Is there any better way to determine initial value?
+	Type last_x = 0.0; // the value one before the last step
+	while (x != last_x) // until the difference is not significant
+	{ // apply Babylonian method step
+		last_x = x;
+		x = (x + s / x) / 2.0;
+	}
+	return x;
 }
-
 // -----------------------------------------------------------------------------
-
 template<typename Type>
 constexpr Type abs(Type s) noexcept {
-  STATIC_ASSERT_IS_ARITHMETRIC(Type);
-  return s < 0 ? -s : s;
+	STATIC_ASSERT_IS_ARITHMETRIC(Type);
+	return s < 0 ? -s : s;
 }
-
 // -----------------------------------------------------------------------------
+
 
 template<typename L, typename R>
 constexpr auto max(L l, R r) noexcept {
-  STATIC_ASSERT_IS_ARITHMETRIC(L);
-  STATIC_ASSERT_IS_ARITHMETRIC(R);
-  return l > r ? l : r;
+	STATIC_ASSERT_IS_ARITHMETRIC(L);
+	STATIC_ASSERT_IS_ARITHMETRIC(R);
+	return l > r ? l : r;
 }
 
 // -----------------------------------------------------------------------------
 
 template<typename L, typename R>
 constexpr auto min(L l, R r) noexcept {
-  STATIC_ASSERT_IS_ARITHMETRIC(L);
-  STATIC_ASSERT_IS_ARITHMETRIC(R);
+	STATIC_ASSERT_IS_ARITHMETRIC(L);
+	STATIC_ASSERT_IS_ARITHMETRIC(R);
 
-  return l < r ? l : r;
+	return l < r ? l : r;
 }
 
 // -----------------------------------------------------------------------------
 
 template<typename Type>
 constexpr bool is_nan(Type t) noexcept {
-  STATIC_ASSERT_IS_ARITHMETRIC(Type);
-  return !(t == t);
+	STATIC_ASSERT_IS_ARITHMETRIC(Type);
+	return !(t == t);
 }
-
 // -----------------------------------------------------------------------------
 
 template<typename Return, typename L, typename R>
 constexpr Return clamp(Return val, L low, R high) noexcept {
-  STATIC_ASSERT_IS_ARITHMETRIC(Return);
-  STATIC_ASSERT_IS_ARITHMETRIC(L);
-  STATIC_ASSERT_IS_ARITHMETRIC(R);
-  if (val < low)
-	return low;
-  else if (val > high)
-	return high;
-  else
-	return val;
+	STATIC_ASSERT_IS_ARITHMETRIC(Return);
+	STATIC_ASSERT_IS_ARITHMETRIC(L);
+	STATIC_ASSERT_IS_ARITHMETRIC(R);
+	if (val < low)
+		return low;
+	else if (val > high)
+		return high;
+	else
+		return val;
 }
 
 // -----------------------------------------------------------------------------
-// Reference :
+
 template<typename Type>
-constexpr Type calc_Pi(std::size_t iteration) noexcept {
-  STATIC_ASSERT_IS_FLOATING_POINT(Type);
-  Type a = 1.0;
-  Type b = 1.0 / sqrt(2.0);
-  Type t = 1.0 / 4.0;
-  Type p = 1.0;
-  Type tmp = 0.0;
-  Type ret;
-  for (std::size_t i = 0; i < iteration; i++) {
-	tmp = a;
-	a = (tmp + b) / 2.0;
-	b = sqrt(tmp * b);
-	t = t - (p * (a - tmp) * (a - tmp));
-	p = 2.0 * p;
-  }
-  return (a + b) * (a + b) / (4.0 * t);
+constexpr Type calc_Pi(int iteration) noexcept {
+	STATIC_ASSERT_IS_FLOATING_POINT(Type);
+	Type a = 1.0;
+	Type b = 1.0 / sqrt(2.0);
+	Type t = 1.0 / 4.0;
+	Type p = 1.0;
+	Type tmp = 0.0;
+	Type ret;
+	for (std::size_t i = 0; i < iteration; i++) {
+		tmp = a;
+		a = (tmp + b) / 2.0;
+		b = sqrt(tmp * b);
+		t = t - (p * (a - tmp) * (a - tmp));
+		p = 2.0 * p;
+	}
+	return (a + b) * (a + b) / (4.0 * t);
 }
+
+// -----------------------------------------------------------------------------
+// Math Constant
+
+template<typename Type = float>
+constexpr Type Pi = calc_Pi<Type>(10);
 
 // -----------------------------------------------------------------------------
 
