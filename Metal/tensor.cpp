9b3428c7e3446c0b757da52a3dc11635a95a7a47
Toru Nagai <nagato0614@gmail.com>
2025-02-09 21:54:48 +0900

    ✨ 新機能(Metal): tensorクラスのヘッダーと実装ファイルを追加

---
+++ Metal/tensor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by toru on 2025/02/09.
+//
+
+#include "tensor.hpp"
---
+++ Metal/tensor.hpp
@@ -0,0 +1,16 @@
+//
+// Created by toru on 2025/02/09.
+//
+
+#ifndef TENSOR_HPP
+#define TENSOR_HPP
+
+
+
+class tensor {
+
+};
+
+
+
+#endif //TENSOR_HPP
